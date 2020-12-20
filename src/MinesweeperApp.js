b705100522b450ba883369c22ee5ae9f7691c330
Jonathan.ybarra <jonatha.ybarra@gmail.com>
2020-12-20 16:13:02 -0300

    complete

---
+++ src/MinesweeperApp.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Board } from './components/Board'
+
+export const MinesweeperApp = () => {
+
+    return (
+        <div>
+            MinesweeperApp
+            <Board/>
+        </div>
+    )
+}
